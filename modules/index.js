dc4643fe6e21ef7e5a2dfecad0717338a588fbef
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-06-11 19:51:51 -0500

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -1,2 +1,9 @@
 Config.history.maxStates = 1;
 Config.history.controls = false;
+
+function appendCallContent() {
+  var paragraph = document.getElementById("call-content");
+  var items = ["hi", "hello", "sup"];
+  var item = items[Math.floor(Math.random() * items.length)];
+  paragraph.innerHTML += item + "<br />";
+}
