dc21e954fafe3f0d3eb2d400f482c9736dbed65e
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-06-13 01:27:26 -0500

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -1,23 +1,3 @@
 Config.history.maxStates = 1;
 Config.history.controls = false;
 
-currentConnected = 0;
-
-function appendCallContent() {
-  var paragraph = document.getElementById("call-content");
-  var items = ["hi", "hello", "sup"];
-  var item = items[Math.floor(Math.random() * items.length)];
-  paragraph.innerHTML += item + "<br />";
-}
-
-function connect(number) {
-  currentConnected = [number]
-  document.getElementsByClassName("connected").innerHTML = "Not Connected";
-  document.getElementsByClassName("connected").className = "disconnected";
-  document.getElementById([number]).className = "connected";
-  document.getElementsByClassName("connected").innerHTML = "Connected";
-}
-
-while true {
-  
-}
